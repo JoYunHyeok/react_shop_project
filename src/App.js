0d2cc7c3cb20efcc0bc4e9d95aa25e14e4c47747
joyh <whfprl10@gmail.com>
2021-05-27 14:14:36 +0900

    Update routing techniques to create pages
    상세페이지를 만들기위해 routing을 적용해보았습니다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Navbar, Nav, NavDropdown, Button, Jumbotron } from 'react-bootstrap';
 import './App.css';
 import { useState } from 'react';
 import Data from './data.js';
+import Detail from './Detail.js'
+
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
@@ -16,8 +19,8 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -29,36 +32,54 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      <Jumbotron className="background">
-        <h1>20% season OFF</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
+      <Switch>
+        <Route exact path="/">
+          <div>main page</div>
+          <Jumbotron className="background">
+            <h1>20% season OFF</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+          </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
 
-      <div className="container">
-        <div className="row">
-          { 
-            shoes.map((a,i)=>{
-             return <Card shoes={shoes[i]} i={i} />
-            })
-          }
-        </div>
-      </div>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((a, i) => {
+                  return <Card shoes={shoes[i]} i={i} />
+                })
+              }
+            </div>
+          </div>
+        </Route>
+
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} ></Detail>
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나</div>
+        </Route>
+
+      </Switch>
+
+
     </div>
   )
 }
 
-function Card(props){
+
+
+function Card(props) {
   return (
     <div className="col-md-4">
-      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%"/>
-      <h4>{ props.shoes.title }</h4>
-      <p>{ props.shoes.content } & { props.shoes.price }</p>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="100%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content} & {props.shoes.price}</p>
     </div>
   )
 }
